5bfdf9550ca6dd87254b346ee6191f0cc18123d5
Pro <luitel.nawaraj12300@gmail.com>
2024-10-04 07:32:30 +0545

    toprated content is updated using useState Hook

--- src/Body.js
+++ src/Body.js
@@ -1,16 +1,30 @@
+import { useState } from "react";
 import { Products } from "../Utils/Products";
 import { Product } from "./Product.js";
+import { useState } from "react";
 
 export const Body=()=>{
+    const [listProduct, setListProduct] = useState(Products);
+
+    const HighRated=()=>{
+        const filterProduct = listProduct.filter(product=>product.rating.rate>=4.5);
+        setListProduct(filterProduct);
+    }
     return(
+        <>
+        <div>
+            <button onClick={HighRated}>Top Rated</button>
+        </div>
         <div className="bodyProduct">
             {
-                Products.map((product,index)=>{
+                listProduct.map((product,index)=>{
                     return(
                         <Product key={product.id} product={product}/>
                     );
                 })
             }
         </div>
+        </>
+        
     );
 }
