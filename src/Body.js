af257578d39de33a73b25f961131cb20ba6c0520
Pro <luitel.nawaraj12300@gmail.com>
2024-10-07 18:44:28 +0545

    Feature of search based on includes is added

--- src/Body.js
+++ src/Body.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-// import { Products } from "../Utils/Products";
+import { Products } from "../Utils/Products";
 import { Product } from "./Product.js";
 import { useState } from "react";
 import { Skeleton } from "./Skeleton.js";
@@ -7,13 +7,11 @@ export const Body=()=>{
     const [listProduct, setListProduct] = useState([]);
     const [orginalProduct, setOriginalProduct] = useState([]);
     const [loading, setLoading] = useState(true);
-
+    const [search, setSearch] = useState([])
 
     useEffect(()=>{
         ProductList();
     },[]);
-
-   
 
     const ProductList = async()=>{
         const data=await fetch('https://fakestoreapi.com/products/')
@@ -22,7 +20,6 @@ export const Body=()=>{
         setOriginalProduct(resJson);
         setLoading(false);
     }
-
 
     const HighRated=async()=>{
         const filterProduct = orginalProduct.filter(product=>product.rating.rate>=4);
@@ -41,6 +38,17 @@ export const Body=()=>{
     return loading?<Skeleton/>:(
         <>
         <div>
+            <div>
+            <input type="text" onChange={(e) => setSearch((e.target.value).toUpperCase)}  value={search}  /> 
+            <button onClick={(e)=>{
+                const searchText= orginalProduct.filter((product)=>{
+                    return product.title.includes(search)
+                });
+                (searchText.length == 0) ? <Skeleton/> : setListProduct(searchText);
+            }}
+            >Search</button>
+
+            </div>
             <button onClick={HighRated}>Top Rated</button>
             <button onClick={LeastRated}>Least Rated</button>
             <button onClick={Reset}>Reset</button>
