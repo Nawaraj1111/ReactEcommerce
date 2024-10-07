770aadbdaba0f3c6cfbce81d68ae050ecef7c526
Pro <luitel.nawaraj12300@gmail.com>
2024-10-07 15:46:37 +0545

    Dynamic fetch is add in top,least and reset product

--- src/Body.js
+++ src/Body.js
@@ -1,22 +1,38 @@
-import { useState } from "react";
-import { Products } from "../Utils/Products";
+import { useEffect, useState } from "react";
+// import { Products } from "../Utils/Products";
 import { Product } from "./Product.js";
 import { useState } from "react";
+import Skeleton from "./Skeleton.js"
 
 export const Body=()=>{
-    const [listProduct, setListProduct] = useState(Products);
+    const [listProduct, setListProduct] = useState([]);
+    const [orginalProduct, setOriginalProduct] = useState([]);
+    
+    useEffect(()=>{
+        ProductList();
+    },[]);
 
-    const HighRated=()=>{
-        const filterProduct = Products.filter(product=>product.rating.rate>=4);
+
+
+    const ProductList = async()=>{
+        const data=await fetch('https://fakestoreapi.com/products/')
+        const resJson = await data.json();
+        setListProduct(resJson);
+        setOriginalProduct(resJson);
+    }
+
+
+    const HighRated=async()=>{
+        const filterProduct = orginalProduct.filter(product=>product.rating.rate>=4);
         setListProduct(filterProduct);
     }
     const LeastRated=()=>{
-        const filterProduct = Products.filter(product=>product.rating.rate<=2);
+        const filterProduct = orginalProduct.filter(product=>product.rating.rate<=2);
         setListProduct(filterProduct);
     }
 
     const Reset=()=>{
-        const filterProduct = Products.filter(product=>(product.rating.rate>=0 && product.rating.rate<=5));
+        const filterProduct = orginalProduct.filter(product=>(product.rating.rate>=0 && product.rating.rate<=5));
         setListProduct(filterProduct);
     }
     return(
